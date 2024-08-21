6d9c78a55a3d316231399e75998555f1d582c4d0
DevAnamTales <Anam-rehman@live.com>
2024-08-21 12:11:55 +0200

    committing to player-feature branch

--- classes/Player.js
+++ classes/Player.js
@@ -1,5 +1,8 @@
 export default class Player {
-  constructor ( name, color ) {
+  constructor(name, color) {
+    if (typeof name !== 'string' || !/^[a-zA-Z]+$/.test(name) || name.length === 0) {
+      throw new Error('Invalid name');
+    }
     this.name = name;
     this.color = color;
   }
