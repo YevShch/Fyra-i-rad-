2fb924eb2eb57b336b862f78f108048dab282e21
YevShch <yevheniiashcherbakova82@gmail.com>
2024-08-24 13:55:10 +0200

    upd tests with prompt

--- classes/App.js
+++ classes/App.js
@@ -33,40 +33,4 @@ export default class App {
         this.playerO = new Player(playerOName, 'O');
     }
 
-    // startGameLoop () {
-    //     while (!this.board.gameOver) {
-    //         // Determine the current player based on the board's current player color
-    //         let player = this.board.currentPlayerColor === 'X' ? this.playerX : this.playerO;
-
-    //         // Prompt user to make a move and get the column input
-    //         let column = prompt(`Ange ditt drag ${player.color} ${player.name} - skriv in kolumn: `);
-
-    //         // Convert input to a number and adjust for zero-based index
-    //         column = +column.trim() - 1;
-
-    //         // Try to make a move, and if not successful, prompt the player again
-    //         const moveMade = this.board.makeMove(player.color, column);
-    //         if (!moveMade) {
-    //             continue;
-    //         }
-
-    //         // Render the board after each move
-    //         this.board.render();
-    //     }
-    // }
-
-    // whoHasWonOnGameOver () {
-    //     // The game is over, determine and announce the result
-    //     console.clear();
-    //     this.board.render();
-
-    //     if (this.board.winner) {
-    //         // If there's a winner, announce the winning player
-    //         let winningPlayer = this.board.winner === 'X' ? this.playerX : this.playerO;
-    //         console.log(`Grattis ${winningPlayer.color}: ${winningPlayer.name} du vann!`);
-    //     } else {
-    //         // If there's no winner, announce a draw
-    //         console.log('Tyvärr det blev oavgjort...');
-    //     }
-    // }
 }
