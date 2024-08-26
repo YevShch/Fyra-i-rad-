d2d3fc395e27d02c0d28352b3dd693fe950caf25
DevAnamTales <Anam-rehman@live.com>
2024-08-26 14:05:37 +0200

    Update App.js
    Updated with whoHasWonOnGameOver method

--- classes/App.js
+++ classes/App.js
@@ -4,7 +4,7 @@ import Player from './Player.js';
 
 export default class App {
 
-    constructor () {
+    constructor() {
         // Infinite loop to repeatedly play the game until the user decides to quit
         while (true) {
             this.createPlayers(); // Initialize players
@@ -15,13 +15,13 @@ export default class App {
             // Ask if the user wants to play again
             console.log('');
             let playAgain = prompt('Vill ni spela igen? (ja/nej)? ');
-            if (playAgain !== 'ja') { 
+            if (playAgain !== 'ja') {
                 break; // Exit the loop if the user doesn't want to play again
             }
         }
     }
 
-    createPlayers () {
+    createPlayers() {
         console.clear(); // Clear the console for a fresh start
         console.log('FOUR-IN-A-ROW\n');
 
@@ -32,5 +32,22 @@ export default class App {
         this.playerX = new Player(playerXName, 'X');
         this.playerO = new Player(playerOName, 'O');
     }
+    // Method to check the game status and return the appropriate message
+    static whoHasWonOnGameOver(board, playerX, playerO) {
+        let winnerName = null;
 
+        if (board.winner === playerX.color) {
+            winnerName = playerX.name;
+        } else if (board.winner === playerO.color) {
+            winnerName = playerO.name;
+        }
+
+        if (winnerName) {
+            return `${winnerName} has won the game!`;
+        } else if (board.isADraw) {
+            return "The game ended in a draw!";
+        } else {
+            return "The game is still ongoing.";
+        }
+    }
 }
