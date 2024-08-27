662f5035d968f75f6970407b58bc4af8a3185638
YevShch <yevheniiashcherbakova82@gmail.com>
2024-08-27 13:11:28 +0200

    changed constructor and added startGame()

--- classes/App.js
+++ classes/App.js
@@ -4,21 +4,10 @@ import Player from './Player.js';
 
 export default class App {
 
-    constructor() {
-        // Infinite loop to repeatedly play the game until the user decides to quit
-        while (true) {
-            this.createPlayers(); // Initialize players
-            this.board = new Board(); // Create a new game board
-            // this.startGameLoop(); // Start the game loop
-            // this.whoHasWonOnGameOver(); // Determine and announce the winner or if it's a draw
-
-            // Ask if the user wants to play again
-            console.log('');
-            let playAgain = prompt('Vill ni spela igen? (ja/nej)? ');
-            if (playAgain !== 'ja') {
-                break; // Exit the loop if the user doesn't want to play again
-            }
-        }
+    constructor () {
+        this.playerX = null; // Initialize player X
+        this.playerO = null; // Initialize player O
+        this.board = null;   // Initialize the game board
     }
 
     createPlayers() {
@@ -48,6 +37,22 @@ export default class App {
             return "The game ended in a draw!";
         } else {
             return "The game is still ongoing.";
+        }
+    }
+
+    startGame () {
+        while ( true ) {
+            this.createPlayers(); // Create players
+            this.board = new Board(); // Create a new game board
+            this.startGameLoop(); // Start the game loop
+            this.whoHasWonOnGameOver(); // Determine and announce the winner or if it's a draw
+
+            // Ask if the user wants to play again
+            console.log( '' );
+            let playAgain = prompt( 'Vill ni spela igen? (ja/nej)? ' ); // Ask if they want to play again
+            if ( playAgain !== 'ja' ) {
+                break; // Exit the loop if the user doesn't want to play again
+            }
         }
     }
 }
