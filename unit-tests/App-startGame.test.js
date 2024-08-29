34eb01ab2df1d73663033bd08e262255821ef578
YevShch <yevheniiashcherbakova82@gmail.com>
2024-08-29 12:19:07 +0200

    Update App-startGame.test.js

--- unit-tests/App-startGame.test.js
+++ unit-tests/App-startGame.test.js
@@ -1,70 +1,31 @@
 import { test, expect, vi } from 'vitest';
-import prompt from './helpers/mockPromptAndConsoleLog.js';
 import App from '../classes/App.js';
-import Board from '../classes/Board.js'
+import Board from '../classes/Board.js';
+import { setMockAnswers } from './helpers/mockPromptAndConsoleLog.js';
 
 
-test("Verify that createPlayers is called when startGame is invoked", () => {
-  
-  // Create a spy on the createPlayers method
-  const appSpy = vi.spyOn(App.prototype, 'createPlayers');
+test( "Verify that startGame method initializes the game correctly", () => {
+  setMockAnswers( 'PlayerX', 'PlayerO', '1', 'nej' )
 
-  // Create an instance of the App class
-  const app = new App(); 
+  const appSpyCreatePlayers = vi.spyOn( App.prototype, 'createPlayers' ).mockImplementation( () => { } );
+  const appSpyStartGameLoop = vi.spyOn( App.prototype, 'startGameLoop' ).mockImplementation( () => { } );
+  const appSpyWhoHasWonOnGameOver = vi.spyOn( App.prototype, 'whoHasWonOnGameOver' ).mockImplementation( () => { } );
+  // vi.spyOn( Board.prototype, 'constructor' ).mockImplementation( () => { } );
+  const boardSpy = vi.spyOn( Board.prototype, 'constructor' ).mockImplementation( () => { } );
 
-  // To call method startGame
+  const app = new App;
   app.startGame();
 
-  // Check that the createPlayers method was called
-  expect(appSpy).toHaveBeenCalled();
-  console.log( 'Pass: createPlayers was called during startGame execution.' );
-} );  
+  expect( appSpyCreatePlayers ).toHaveBeenCalled();
+  console.log( "Pass: createPlayers() is called when startGame() is invoked" )
 
+  expect( appSpyStartGameLoop ).toHaveBeenCalled();
+  console.log( "Pass: startGameLoop() is called when startGame() is invoked" )
 
+  expect( appSpyWhoHasWonOnGameOver ).toHaveBeenCalled();
+  console.log( "Pass: whoHasWonOnGameOver() is called when startGame() is invoked" )
 
-test("Verify that Board instance is created when startGame is invoked", () => {
-// Create an instance of the App class
-const app = new App();
-
-// Mock the Board constructor to avoid actual creation
-vi.spyOn( Board.prototype, 'constructor' ).mockImplementation( () => { } );
-
-// Call the startGame method
-app.startGame();
-
-// Check that the board property is an instance of Board
-expect( app.board ).toBeInstanceOf( Board );
-console.log( 'Pass: Board instance was created and assigned to app.board during startGame execution.' );
-});
-
-
-test( "Verify that startGameLoop is called when startGame is invoked", () => {
-  // Create a spy on the startGameLoop method
-  const appSpy = vi.spyOn( App.prototype, 'startGameLoop' );
-
-  // Create an instance of the App class
-  const app = new App(); // This should trigger the createPlayers method call
-
-  // To call method startGame
-  app.startGame();
-
-  // Check that the startGameLoop method was called
-  expect( appSpy ).toHaveBeenCalled();
-  console.log( 'Pass: startGameLoop was called during startGame execution.' )
-} );  
-
-
-test( "Verify that whoHasWonOnGameOver is called when startGame is invoked", () => {
-  // Create a spy on the startGameLoop method
-  const appSpy = vi.spyOn( App.prototype, 'whoHasWonOnGameOver' );
-
-  // Create an instance of the App class
-  const app = new App(); 
-
-  // To call method startGame
-  app.startGame();
-
-  // Check that the whoHasWonOnGameOver method was called
-  expect( appSpy ).toHaveBeenCalled();
-  console.log( 'Pass: whoHasWonOnGameOver was called during startGame execution.' )
-} ); 
+  expect( app.board ).toBeInstanceOf( Board );
+  console.log( "Pass: app.board is an instance of Board" );
+  // expect( boardSpy ).toHaveBeenCalled();
+} )
