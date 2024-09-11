3bbc7c9a5ffeef2ec61f60d0c2f23a4806cf8e1d
YevShch <yevheniiashcherbakova82@gmail.com>
2024-09-11 10:18:41 +0200

    Create players-initialization.js

---
+++ specs/step_definitions/players-initialization.js
@@ -0,0 +1,69 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given( 'the game has started', () => {
+  cy.visit( 'http://localhost:5173/' );
+} );
+
+When('the system starts', () => {
+  cy.contains( 'CONNECT FOUR' ).should( 'be.visible' );
+});
+
+When( 'I enter {string} as the name of Player 1', ( name ) => {
+  cy.get( 'input[name="answer"]' )
+    .type( name )
+    .should( 'have.value', name )
+    .type( '{enter}' );
+} ); 
+
+When( 'the system asks for the name of Player 1', () => {
+  cy.contains( 'Enter the name of player' ).should( 'be.visible' );
+} );
+
+
+Then('the system should ask for the name of Player 1', () => {
+  cy.contains( 'Enter the name of player' ).should( 'be.visible' );
+});
+
+Then( 'the system asks for the name of Player 2', () => {
+  cy.contains( 'Enter the name of player' ).should( 'be.visible' );
+} );
+
+Then( 'I enter {string} as the name of Player 2', ( name ) => {
+  cy.get( 'input[name="answer"]' )
+    .type( name )
+    .should( 'have.value', name )
+    .type( '{enter}' );
+} );
+
+
+Then('the system should display the message {string}', (message) => {
+  cy.contains( message ).should( 'be.visible' );
+});
+
+Then( 'the game should display {string} with a red circle indicating it is Player 1\'s turn', ( turnMessage ) => {
+  cy.get( '.color-circle.red-circle' ).should( 'exist' );
+  cy.contains( turnMessage ).should( 'be.visible' );
+} );
+
+
+// When('I enter {string} as the name of Player 1', (invalidName) => {
+//   cy.get( 'input[name="answer"]' )
+//     .type( invalidName )
+//     .should( 'have.value', invalidName )
+//     .type( '{enter}' );
+// });
+
+Then('the system should re-ask for the name of Player 1', () => {
+  cy.contains( 'Enter the name of player' ).should( 'be.visible' );
+});
+
+// When('I enter {string} as the name of Player 1', (validName) => {
+//   cy.get( 'input[name="answer"]' )
+//     .type( validName )
+//     .should( 'have.value', validName )
+//     .type( '{enter}' );
+// });
+
+Then('the system should accept the name and ask for the name of Player 2', () => {
+  cy.contains( 'Enter the name of player' ).should( 'be.visible' );
+});
