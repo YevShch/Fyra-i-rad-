e9c8933777d30ad8504fec51e4edfa655f12d600
DevAnamTales <Anam-rehman@live.com>
2024-09-12 12:05:20 +0200

    Create registerPlayers.js
    Changed player x and o with our player red and yellow

---
+++ tests/helpers/commonTasks/registerPlayers.js
@@ -0,0 +1,23 @@
+import { expect } from 'vitest';
+import getDocument from '../mock-help/getDocument.js';
+import waitUntil from '../mock-help/waitUntil.js';
+
+import App from '../../../classes/App.js';
+
+// make the program sleep less (see classes/helpers/sleep.js)
+globalThis.mockMinimalSleep = true;
+
+// in several test we want to register the players by entering their names
+// do this (including creating a new App) and return the body after its done
+export default async function registerPlayers () {
+  let { body } = getDocument();
+  globalThis.mockAnswers = [ 'Anna', 'Beata' ];
+  let app = new App();
+  // wait until the dom does not have p tag in the main tag with 'Enter names'
+  await waitUntil( () =>
+    !body.querySelector('main p').innerText.includes( 'Waiting for player names...' ) );
+  // has the app registered the players
+  expect(app.playerRed.name ).toBe( 'Anna' );
+  expect(app.playerYellow.name ).toBe( 'Beata' );
+  return body;
+}
