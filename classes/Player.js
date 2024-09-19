fda195befc71aaf77a2771e3ff401b06e0245c6d
YevShch <yevheniiashcherbakova82@gmail.com>
2024-09-19 10:38:42 +0200

    Сreated the first version of logic for a smart bot

--- classes/Player.js
+++ classes/Player.js
@@ -11,7 +11,7 @@ export default class Player {
   }
 
   async makeBotMove () {
-    console.log( 'makeBotMove has been called' )
+    console.log( 'makeBotMove has been called' );
     await sleep( 500 ); // Delay for bot's "thinking" to simulate human-like behavior
     let column;
     if ( this.type === 'A dumb bot' ) {
@@ -27,28 +27,46 @@ export default class Player {
     return [ shuffleArray( this.legalMoves )[ 0 ] ];
   }
 
-
   makeSmartBotMove () {
-    let orgState = this.state(); // Get the current state of the board
+    console.log( 'Matrix BEFORE a temporary move', this.board.matrix );
+
+    // orgState - the current state on the board
+    let orgState = this.state();
+    console.log( 'Current state', orgState );
+
+    // store scores for each possible move in scores
     let scores = [];
 
-    // Loop through each legal move
+    // loop through/try each legal/possible move
     for ( let column of this.legalMoves ) {
-      // Find the lowest empty row in this column
-      let row = this.board.matrix.findIndex( r => r[ column ].color === ' ' );
-      if ( row === -1 ) continue; // Skip if no valid row is found
+      // Find the lowest empty cell in a column
+      let row = -1;
+      for ( let r = this.board.matrix.length - 1; r >= 0; r-- ) {
+        if ( this.board.matrix[ r ][ column ].color === ' ' ) {
+          row = r;
+          break;
+        }
+      }
 
-      // Temporarily make the move
-      this.board.matrix[ row ][ column ].color = this.color;
-      let futureState = this.state(); // Get the state of the board after the move
-      this.board.matrix[ row ][ column ].color = ' '; // Undo the temporary move
+      if ( row === -1 ) continue; // If there is no empty cell, skip the column
 
-      // Score this move based on the future state
+      let cell = this.board.matrix[ row ][ column ];
+      cell.color = this.color; // make temporary move
+      console.log( 'Cell I would change', cell );
+      console.log( 'Matrix AFTER temporary move', this.board.matrix );
+
+      let futureState = this.state(); // the state if we made this move
+      cell.color = ' '; // undo temporary move
+
+      console.log( 'IF I MADE THE MOVE', column );
+      console.log( 'THE NEW STATE WOULD BE', futureState );
+
+      // remember the score for this possible move
       scores.push( { column, score: this.score( orgState, futureState ) } );
     }
 
-    // Sort scores in descending order and select the best move
-    scores = shuffleArray( scores ).sort( ( a, b ) => b.score - a.score );
+    scores = shuffleArray( scores ).sort( ( a, b ) => a.score > b.score ? -1 : 1 );
+    console.log( 'Scores:', scores );
     let { column } = scores[ 0 ];
     return [ column ];
   }
@@ -56,36 +74,42 @@ export default class Player {
   score ( orgState, futureState ) {
     // Define priorities for scoring moves
     let priorities = [
-      { me: 3 }, { opp: 2 }, { opp: 1 }, { me: 2 }, { me: 1 }
+      { me: 4 }, { opp: 3 }, // Maximum priority: winning or blocking a win
+      { me: 3 }, { opp: 2 }, // Good: creating a line of 3
+      { me: 2 }, { opp: 1 }, // Intermediate: creating a line of 2
+      { me: 1 } // Less important: placing 1 piece
     ];
+
+    // score variable - which we will use to calculate a score
     let score = 0;
 
-    // Evaluate each winning combination
+    // loop through each part of the states, corresponding to a winCombo
     for ( let i = 0; i < orgState.length; i++ ) {
+      // short aliases for each orgState and futureState part
+      // b - before/orgState, a - after/futureState
       let b = orgState[ i ], a = futureState[ i ];
-      if ( b.me === a.me && b.opp === a.opp ) continue; // No change, skip
-      if ( b.me > 0 && b.opp > 0 ) continue; // Both players have pieces, skip
-
+      // no change in winCombo - not interesting
+      if ( b.me === a.me && b.opp === a.opp ) { continue; }
+      // winCombo can't be won by either player (both already have pieces in place)
+      if ( b.me > 0 && b.opp > 0 ) { continue; }
+      // there has been change in this winCombo, so I must have added a piece
       let partScore = '';
-      // Evaluate how good the move is for a single winCombo
+      // partScore is how good the move is for ONE winCombo
+      // partScore will become number of different priorities x 2 long
+      // initially it is a string, but we will convert to a number before
+      // adding to the total score
       for ( let j = 0; j < priorities.length; j++ ) {
         let key = Object.keys( priorities[ j ] )[ 0 ];
         let value = priorities[ j ][ key ];
-        partScore += ( a[ key ] === value ) ? '01' : '00';
+        if ( a[ key ] === value ) { partScore += '01'; }
+        else { partScore += '00'; }
       }
       score += +partScore;
     }
 
-    // (the scoreing works well in Tic-Tac-Toe
-    // but in Connect 4 it misses that what it considers the best move (highest score)
-    // will sometimes give the opponent an opportunity to win by playing the same column
-    // (ie. directly "above") the chosen move
-    // you can avoid this by trying to play an opponent move in the same column
-    // and if that gives a win set score to negative - 1)
-
-    return score; // Return the total score for the move
+    // Return the total score for the move
+    return score;
   }
-
 
   get legalMoves () {
     let moves = [];
