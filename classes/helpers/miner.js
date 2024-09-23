4f8d2215be93b7776dd9ce504b33edf6cc21ab26
YevShch <yevheniiashcherbakova82@gmail.com>
2024-09-23 10:31:12 +0200

    changed the getMoveFromExternalAI function

--- classes/helpers/miner.js
+++ classes/helpers/miner.js
@@ -6,7 +6,7 @@
 // - spelare 1 spelade kolumn 4, 
 // - sedan spelade spelare 2 kolumn 1, 
 // - sedan spelade spelare 1 kolumn 3
-export async function getMoveFromExternalAI ( aiLevel = 6, state ) {
+export async function getMoveFromExternalAI ( aiLevel = ' ', state ) {
   let response = await ( await fetch( 'https://ludolab.net/solve/connect4?level=' + aiLevel + '&position=' + state ) ).json();
   let movesSortedByHowGood = response.sort( ( a, b ) => a.score > b.score ? -1 : 1 );
   return movesSortedByHowGood[ 0 ].move;
