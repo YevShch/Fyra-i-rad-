e6fa28a7004631f0647edfec8ae52a4fda933adf
YevShch <yevheniiashcherbakova82@gmail.com>
2024-09-30 10:54:43 +0200

    Update App.js

--- classes/App.js
+++ classes/App.js
@@ -9,6 +9,10 @@ import createConfetti from './helpers/createConfetti.js';
 export default class App {
   constructor ( playerRed, playerYellow, whoStarts = 'red' ) {
     try {
+      // network related properties
+      this.networkPlay = networkPlay;
+      this.myColor = myColor; // note: only used in network play 
+
       this.dialog = new Dialog();
       this.board = new Board( this ); // Always create a new Board instance
       this.board.currentPlayerColor = whoStarts;
@@ -25,6 +29,11 @@ export default class App {
         // start the new game
         this.namesEntered = true;
         this.board.initiateBotMove();
+        // if network play, then replace the listener 
+        // (that belongs to the old app / game) with a new one
+        if ( networkPlay ) {
+          Network.replaceListener( obj => this.networkListener( obj ) );
+        }
       } else {
         // enter new players
         this.askForNamesAndTypes();
@@ -37,8 +46,87 @@ export default class App {
     }
   }
 
+
+  async askIfNetworkPlay () {
+    this.networkPlay = ( await this.dialog.ask(
+      `Network Play: Do you want to play<br>against a friend via the Internet?`, [ 'Yes', 'No' ] ) ) === 'Yes';
+    await sleep( 500 );
+    if ( !this.networkPlay ) { return; }
+    let startNetworkPlay = ( await this.dialog.ask(
+      'Do you want to create a new network game? Or join one?', [ 'Create', 'Join' ] ) ) === 'Create';
+    await sleep( 500 );
+    let name = await this.dialog.ask( 'Enter your name:' );
+    await sleep( 500 );
+    if ( startNetworkPlay ) {
+      this.myColor = 'red';
+      let code = generateCode();
+      Network.startConnection( name, code, obj => this.networkListener( obj ) );
+      let extra = '';
+      while ( !this.bothNetworkPlayersHasJoined ) {
+        await this.dialog.ask(
+          `Send the following join code to your friend:<br>
+          <input type="text" name="joinCode" readonly value="${ code }">${ extra }`, [ 'OK' ] );
+        extra = '<br>Waiting for your friend to join...'
+        await sleep( 500 );
+      }
+    }
+    else {
+      this.myColor = 'yellow';
+      let extra = '';
+      while ( !this.bothNetworkPlayersHasJoined ) {
+        let code = await this.dialog.ask( `Enter a the join code you got from your friend:${ extra }` );
+        this.joiners = [];
+        this.enteredJoinCode = code;
+        Network.startConnection( name, code, obj => this.networkListener( obj ) );
+        extra = '<br>Incorrect join code... Try again...';
+        await sleep( 500 );
+      }
+    }
+    // create players
+    this.playerX = new Player( this.joiners.shift(), 'Human', 'red', this.board );
+    this.playerO = new Player( this.joiners.shift(), 'Human', 'yellow', this.board );
+    this.namesEntered = true;
+    this.render();
+  }
+
+  networkListener ( { user, timestamp, data } ) {
+    // keep this console.log until you understand how 
+    // and which network messages are sent
+    console.log( user, timestamp, data );
+
+    // wait for both players to join
+    this.joiners = this.joiners || [];
+    if ( user === 'system' && data.includes( 'joined channel' ) ) {
+      this.joiners.push( data.split( ' ' )[ 1 ] );
+      this.bothNetworkPlayersHasJoined = this.joiners.length >= 2;
+    }
+
+    // remove dialog/modal for player X when player O has joined
+    if ( this.myColor === 'red'
+      && this.bothNetworkPlayersHasJoined
+      && document.querySelector( 'dialog input[name="joinCode"]' )
+    ) {
+      let okButton = document.querySelector( 'dialog .button.OK' );
+      okButton && okButton.click();
+    }
+
+    // make move sent to us from opponent via the network
+    if ( data.color && data.color !== this.myColor ) {
+      let { color, column } = data;
+      this.board.makeMove( color, column, false ) && this.render();
+    }
+
+    // if playAgain sent to player O from player X, playAgain
+    if ( this.myColor === 'yellow' && data.action === 'playAgain' ) {
+      globalThis.playAgain();
+    }
+  }
+
+
   async askForNamesAndTypes ( color = 'red' ) {
     try {
+      color === 'red' && await this.askIfNetworkPlay();
+      if ( this.networkPlay ) { return; }
       const okName = name => name.match( /[a-zåäöA-ZÅÄÖ]{2,}/ );
       let playerName = '';
       let playerType = '';
@@ -48,11 +136,16 @@ export default class App {
           <div class="prompt-text">Enter the name of player ${ this.generateColorCircle( color ) }:</div>
         `;
         playerName = await this.dialog.ask( promptHtml );
-        await sleep( 500 );
-        playerType = await this.dialog.ask(
-          `Which type of player is ${ playerName }?`,
-          [ 'Human', 'A dumb bot', 'A smart bot' ]
-        )
+        if ( !this.networkPlay ) {
+          await sleep( 500 );
+          playerType = await this.dialog.ask(
+            `Which type of player is ${ playerName }?`,
+            [ 'Human', 'A dumb bot', 'A smart bot' ]
+          )
+        }
+        else {
+          playerType = 'Human';
+        }
       }
 
       // Create a player object
@@ -60,16 +153,12 @@ export default class App {
       console.log( `Created ${ color } player: ${ playerName }` );
 
       // If the red player's name is entered, prompt for the yellow player's name
-      if ( color === 'red' ) { this.askForNamesAndTypes( 'yellow' ); return; }
+      if ( color === 'red' && !this.networkPlay ) { this.askForNamesAndTypes( 'yellow' ); return; }
      
         // Once both names are entered, set the flag and re-render 
         this.namesEntered = true;
         this.render();  // Re-render with names and player details
         this.board.initiateBotMove();
-
-        // make players global for debugging
-        globalThis.playerRed = this.playerRed;
-        globalThis.playerYellow = this.playerYellow;
       
     } catch ( error ) {
       console.error( 'Error during name entry:', error );
@@ -117,6 +206,13 @@ export default class App {
         </div>
       `;
 
+      if ( this.networkPlay && this.myColor !== this.board.currentPlayerColor ) {
+        document.body.classList.add( 'notMyTurn' );
+      }
+      else {
+        document.body.classList.remove( 'notMyTurn' );
+      }
+
       // Create confetti if the game is over and there's a winner
       if ( this.board.gameOver && this.board.winner ) {
         createConfetti();
@@ -130,6 +226,9 @@ export default class App {
   renderQuitButton () {
     try {
       if ( !this.namesEntered ) { return ''; }
+
+      // don't show button for the joining player during network play
+      if ( this.networkPlay && this.myColor === "O" ) { return ''; }
 
       globalThis.quitGame = async () => {
         let answer = await this.dialog.ask( 'What do you want to do?',
@@ -152,23 +251,22 @@ export default class App {
     globalThis.playAgain = async () => {
       // Ensure that the board and state are fully reset before starting a new game
       let playerToStart = this.whoStarts === 'red' ? this.playerYellow : this.playerRed;
-
-      // Ask which player starts
-      await this.dialog.ask(
-        `IT'S ${ this.namePossessive( playerToStart.name ).toUpperCase() } TURN TO START!`, [ 'OK' ]
-      );
-
-      // Create a fresh game instance with the same players but a new board
-      const newGameApp = new App( this.playerRed, this.playerYellow, playerToStart.color );
-
-      // Render the new game
-      newGameApp.render();
+      // if network  and player X, then send 'playAgain' to player O
+      if ( this.networkPlay && this.myColor === 'red' ) {
+        Network.send( { action: 'playAgain' } );
+      }
+      new App( this.playerRed, this.playerYellow, playerToStart.color,
+        this.networkPlay, this.myColor );
     }
 
     globalThis.newPlayers = () => new App();
   }
 
   renderPlayAgainButtons () {
+
+    // don't show buttons for the joining player during network play
+    if ( this.networkPlay && this.myColor === 'O' ) { return ''; }
+
     return /*html*/`
       <div class="button" onclick="playAgain()">Replay</div>
       <div class="button" onclick="newPlayers()">New game</div>
