65348f47c59507b9a9d5320fa31b3613a6057985
Esra Unal <118307139+esraunal07@users.noreply.github.com>
2024-10-07 11:11:26 +0200

    Replay and Startakorrekt upd.

--- specs/NW-StartaSpeletKorrekt.feature
+++ specs/NW-StartaSpeletKorrekt.feature
@@ -3,9 +3,6 @@ Feature: Fyra i Rad Multiplayer over Network
   They should be able to create or join a game, take turns, and start playing.
 
   Scenario: Two players start a game over the network
-    Given that Player1 chooses to create a new game
-    And Player2 chooses to join the game
-    When Player1 sends the join code to Player2
-    And Player2 enters the join code and joins the game
-    Then both players should be registered in the game
-    And Player1 should take the first turn
+    When One of the two players starts the game, the other joins the game
+    Then The game is played until one of the players wins.
+ 
--- specs/step_definitions/NW-Replay.js
+++ specs/step_definitions/NW-Replay.js
@@ -1,48 +1,54 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { getIframeBody } from "../helpers/iframes.js";
 
-Given('the game has finished with a winner in network play', () => {
-  // Oyunun network modunda başlatıldığından emin ol
-  cy.visit('/index.html'); // Oyunu başlat
+// Given One of the two players starts the game and the other joins the game
+Given('Redplayer starts the game Yellowplayer joins the game', () => {
+  cy.visit('/iframed-network-play.html');
+
+  // Player Red starts the game
+  getIframeBody('iframe#playerRed').find('.button.Yes').click();
+  getIframeBody('iframe#playerRed').find('.button.Create').click();
+  getIframeBody('iframe#playerRed').find('input[name="answer"]').type('Nile{enter}');
   
-  // Ağa bağlan ve iki oyuncu arasındaki oyunu simüle et
-  // "Create" seçeneğini tıklayıp oyunu başlatıyoruz
-  cy.contains('Yes').click(); // Network play seçeneği
-  cy.contains('Create').click(); // Oyun oluşturuluyor
-  cy.get('input').type('Player1'); // Oyuncu adını giriyoruz
-  cy.contains('OK').click();
+  // Retrieve the join code
+  getIframeBody('iframe#playerRed').find('input[name="joinCode"]').then((element) => {
+    const joinCode = element.val(); // Store the join code
 
-  // İkinci oyuncu simülasyonu: Aynı kodla katılacak oyuncu üzerinden ilerleyebiliriz
-  cy.window().then((win) => {
-    // Aşağıda her iki oyuncunun tıklamaları ve hamleleri yer alıyor
-    // Birkaç hamle yaparak oyunu sona erdiriyoruz
-    cy.get('.column').eq(0).click(); // Kırmızı oyuncu hamlesi
-    cy.get('.column').eq(1).click(); // Sarı oyuncu hamlesi
-    cy.get('.column').eq(0).click(); // Kırmızı oyuncu hamlesi
-    cy.get('.column').eq(1).click(); // Sarı oyuncu hamlesi
-    cy.get('.column').eq(0).click(); // Kırmızı oyuncu hamlesi
-    cy.get('.column').eq(1).click(); // Sarı oyuncu hamlesi
-    cy.get('.column').eq(0).click(); // Kırmızı oyuncu hamlesi, oyunu kazanır
+    // Player Yellow joins the game
+    getIframeBody('iframe#playerYellow').find('.button.Yes').click();
+    getIframeBody('iframe#playerYellow').find('.button.Join').click();
+    getIframeBody('iframe#playerYellow').find('input[name="answer"]').type('Tuna{enter}');
+    getIframeBody('iframe#playerYellow').find('dialog:contains("join code") input[name="answer"]').type(joinCode + '{enter}');
+    cy.wait(1000);
   });
 });
+When('both players play the game until one wins', () => {
+  for (let i = 0; i < 4; i++) {
+    getIframeBody('iframe#playerRed').find('.cell.empty[data-column="1"]').first().should('be.visible').click({ force: true }); // Player Red's move
+    cy.wait(1000);
+    getIframeBody('iframe#playerYellow').find('.cell.empty[data-column="2"]').first().should('be.visible').click({ force: true }); // Player Yellow's move
+    cy.wait(1000);
+  }
+});
+// And the winner should be visible on both screens
+Then('the winner should be visible on both screens', () => {
+  getIframeBody('iframe#playerRed').find('.player-name')
+  .should('be.visible')
+  .and('have.text', 'Nile won!');
+cy.wait(1000);
 
-Then('a Replay button should be visible for the player who started the game', () => {
-  // Replay butonunun görünür olduğunu doğrula
-  cy.get('.button').contains('Replay').should('be.visible');
+getIframeBody('iframe#playerYellow').find('.player-name')
+.should('be.visible')
+.and('have.text', 'Nile won!');
+cy.wait(1000);
 });
 
-When('the player clicks on the Replay button', () => {
-  // Replay butonuna tıklıyoruz
-  cy.get('.button').contains('Replay').click();
+// When the "Replay" button can be clicked on the Red Player's screen
+When('the "Replay" button can be clicked on the Red Player\'s screen', () => {
+  getIframeBody('iframe#playerRed').find('.button').contains('Replay')
+    .should('be.visible')
+    .and('not.be.disabled')
+    .click();
 });
 
-Then('the game should restart in network mode', () => {
-  // Oyun yeniden başlatılmalı
-  cy.get('.player-name').should('contain', "Player1's turn");
-});
 
-Then('the board should be empty', () => {
-  // Tahtanın boş olduğundan emin oluyoruz
-  cy.get('.column').each(($column) => {
-    cy.wrap($column).find('.slot').should('not.have.class', 'red').and('not.have.class', 'yellow');
-  });
-});
